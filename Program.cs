2f8f164154fbde2b0d43fb430894dc08a40b5a1f
Saad Savari <savaris@wwt.com>
2021-03-04 11:03:38 +0000

    work without license
    add readme.md
    
    cleanup

--- Program.cs
+++ Program.cs
@@ -4,16 +4,16 @@ namespace AsposeBug
 {
     class Program
     {
-    static void Main(string[] args)
-    {
+        static void Main(string[] args)
+        {
             using (MemoryStream ms = new MemoryStream())
-            {                
+            {
                 ImportLicense();
                 var wrdf = new Aspose.Words.Document("./input.html");
                 string Dir = "./output/";
-                if(Directory.Exists(Dir))
+                if (Directory.Exists(Dir))
                     Directory.Delete(Dir, true);
-                
+
                 wrdf.Save(Dir + "output.pdf", Aspose.Words.SaveFormat.Pdf);
                 wrdf.Save(Dir + "output.jpeg", Aspose.Words.SaveFormat.Jpeg);
                 wrdf.Save(Dir + "output.html", Aspose.Words.SaveFormat.Html);
@@ -21,31 +21,35 @@ namespace AsposeBug
             }
         }
 
-        public static void ImportLicense(){
-                var asposeLicenseValue = File.ReadAllText("./license.txt");
-                
-                using var stream = new MemoryStream();
+        public static void ImportLicense()
+        {
+            var asposeLicenseValue = File.ReadAllText("./license.txt");
 
-                using var write = new StreamWriter(stream);
+            if (string.IsNullOrEmpty(asposeLicenseValue))
+                return;
 
-                write.Write(asposeLicenseValue);
-                write.Flush();
-                stream.Position = 0;
+            using var stream = new MemoryStream();
 
-                var docLicense = new Aspose.Words.License();
-                docLicense.SetLicense(stream);
+            using var write = new StreamWriter(stream);
 
-                stream.Position = 0;
-                var pdfLicense = new Aspose.Pdf.License();
-                pdfLicense.SetLicense(stream);
+            write.Write(asposeLicenseValue);
+            write.Flush();
+            stream.Position = 0;
 
-                stream.Position = 0;
-                var imagingLicense = new Aspose.Imaging.License();
-                imagingLicense.SetLicense(stream);
+            var docLicense = new Aspose.Words.License();
+            docLicense.SetLicense(stream);
 
-                stream.Position = 0;
-                var htmlLicense = new Aspose.Html.License();
-                htmlLicense.SetLicense(stream);
+            stream.Position = 0;
+            var pdfLicense = new Aspose.Pdf.License();
+            pdfLicense.SetLicense(stream);
+
+            stream.Position = 0;
+            var imagingLicense = new Aspose.Imaging.License();
+            imagingLicense.SetLicense(stream);
+
+            stream.Position = 0;
+            var htmlLicense = new Aspose.Html.License();
+            htmlLicense.SetLicense(stream);
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+# Aspose License
+> Add Aspose license to license.txt file
